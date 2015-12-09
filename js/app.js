aae82ddb6a4f83852d197a2d7f8e15ea2e07db4e
adesanto <aarondesanto@gmail.com>
2015-12-08 22:20:36 -0500

    map moved to seperate view, login view added and populated with content, some rough angular fleshed out

--- js/app.js
+++ js/app.js
@@ -1,57 +1,23 @@
-function initMap() {
-  var pointA = new google.maps.LatLng(42.344836, -83.034109),
-      pointB = new google.maps.LatLng(42.358775, -82.999048),
-      waypts = [
-                  {
-                    location: new google.maps.LatLng(42.339094, -83.030054),
-                    stopover: false
-                  },
-                  {
-                    location: new google.maps.LatLng(42.346977, -83.012180),
-                    stopover: false
-                  }
-              ],
-      myOptions = {
-        zoom: 7,
-        center: pointA
-      },
-      map = new google.maps.Map(document.getElementById('map'), myOptions),
-      // Instantiate a directions service.
-      directionsService = new google.maps.DirectionsService,
-      directionsDisplay = new google.maps.DirectionsRenderer({
-        map: map
-      }),
-      markerA = new google.maps.Marker({
-        position: pointA,
-        title: "point A",
-        label: "A",
-        map: map
-      }),
-      markerB = new google.maps.Marker({
-        position: pointB,
-        title: "point B",
-        label: "B",
-        map: map
-      });
+var app = angular.module("myApp", ["ngRoute"]);
 
-  // get route from A to B
-  calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB, waypts);
-}
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: "partials/rides.html",
+      controller: "maincontroller"
+    })
+    .when("/login", {
+      templateUrl: "partials/login.html",
+      controller: "maincontroller"
+    })
+    .otherwise({
+      templateUrl: "partials/home.html",
+      controller: "maincontroller"
+    });
 
-function calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB, waypts) {
-  directionsService.route({
-    origin: pointA,
-    destination: pointB,
-    waypoints: waypts,
-    optimizeWaypoints: true,
-    travelMode: google.maps.TravelMode.DRIVING
-  }, function(response, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(response);
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
-}
+  $locationProvider.html5Mode(true);
+});
 
-initMap();
+(function() {
+  $(".phone-screen").backstretch("https://projectmillennial.files.wordpress.com/2012/10/6944328029_ad851f454f_h2.jpg");
+})();
